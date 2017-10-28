87d72a2eb0197a54f2cc43685e32a8e8b20b88c2
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-28 21:33:22 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,10 +16,10 @@ class Scraper
     end
   end
 
-  def get_page
+  def get_page 
+    #Use Nokogiri + open-uri to grab whole HTML doc from page
   end
+
   
+
 end
-
-
-
