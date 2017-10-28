f65a58cac1f8845cf6b04cecbec6d0bbbb25667a
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-28 21:36:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,14 +5,28 @@ require_relative './course.rb'
 
 class Scraper
 
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+  def get_page
+    # Use Nokogiri + open-uri to grab whole HTML doc from page
+  end
+
+  def get_courses
+    # Use CSS selector to grab HTML elements that contain a course
+    # Returns collection of courses (XML elements)
+  end
+
+  def make_courses
+    # Instantiates Course objects
+    # Gives correct title, schedule, description attributes
+  end
+
+  def print_courses
+  self.make_courses
+  Course.all.each do |course|
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
     end
   end
+end
 end
