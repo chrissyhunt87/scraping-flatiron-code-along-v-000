272c27641345b709bafc75206b99f7067d34f568
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-28 21:33:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,10 +16,10 @@ class Scraper
     end
   end
 
-  def get_page 
-    #Use Nokogiri + open-uri to grab whole HTML doc from page
+  def get_page
+    # Use Nokogiri + open-uri to grab whole HTML doc from page
   end
 
-  
+
 
 end
